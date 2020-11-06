eb59f63ffe2227816e6726a50356a6c36ca7e11a
sailomsaengdaed <emmawat_patt@hotmail.com>
2020-11-06 14:49:07 +0700

    SIP2
    Library SIP2 For .net standard
    User C# to Develop

---
+++ SIP2/SipExceptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIP2
+{
+    public class ConnectionFailedException : Exception
+    {
+        public ConnectionFailedException() { }
+        public ConnectionFailedException(string message) : base(message) { }
+    }
+
+    public class HandshakeFailedException : Exception
+    {
+        public HandshakeFailedException() { }
+        public HandshakeFailedException(string message) : base(message) { }
+    }
+
+    public class NotConnectedException : Exception
+    {
+        public NotConnectedException() { }
+        public NotConnectedException(string message) : base(message) { }
+    }
+
+    public class InvalidParameterException : Exception
+    {
+        public InvalidParameterException() { }
+        public InvalidParameterException(string message) : base(message) { }
+    }
+
+    public class NoChecksumException : Exception
+    {
+        public NoChecksumException() { }
+        public NoChecksumException(string message) : base(message) { }
+    }
+}
